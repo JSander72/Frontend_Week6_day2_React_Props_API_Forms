43871118b3fb3cd27a2edf1961527c8ef8b8e907
JSander72 <jamesearlsanders2@outlook.com>
2024-10-01 21:47:43 -0400

    final

---
+++ src/components/AxiosUserList.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import ReusableButton from './ReusableButton'
+import axios from 'axios'
+
+const AxiosUserList = () => {
+  // Initializing the users state with an empty array
+  const [users, setUsers] = useState([])
+  const [selectedUser, setSelectedUser] = useState(null)
+  const [selectedUserInfo, setSelectedUserInfo] = useState(null)
+
+  const deleteUser = (index) => {
+    const newUsers = users.filter((user, i) => i !== index)
+    setUsers(newUsers)
+  }
+
+  const selectUser = (id) => {
+    setSelectedUser(id)
+    getUserInfo(id)
+  }
+  const getUserInfo = (id) => {
+    axios.get(`https://fakestoreapi.com/users/${id}`)
+    .then(response => {
+      console.log(response)
+      setSelectedUserInfo(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+    }
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/users/')
+    .then(response => {
+
+        console.log(response)
+
+        setUsers(response.data)
+    } )
+    .catch(error => {
+        console.log(error)
+    })
+  }, [])
+
+  return (
+    <div>
+      <h1>List of Users</h1>
+
+      { selectedUser && 
+        <div>
+          <h3>Selected User</h3>
+          <p>{selectedUser}</p>
+        </div>
+      }
+
+      { selectedUserInfo && 
+        <div>
+            <h3>Selected User</h3>
+            <p>{selectedUser.name.firstname}</p>
+            <p>{selectedUser.name.lastname}</p>
+            <p>{selectedUser.name.email}</p>
+        </div>
+      }
+
+      <h3>Users</h3>
+
+      { users.map((user, index) => 
+        <div key={index}>
+          <p>{user.name.firstname}</p>
+
+          <ReusableButton handleClick={() => deleteUser(index)} title="Delete User"/>
+          <br />
+          <ReusableButton handleClick={() => selectUser(user.id)} title="Select User"/>
+          <button onClick={() => setSelectedUser(user.id)}>Third Button</button>  
+
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AxiosUserList
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ReusableButton from './ReusableButton'
 
-const UserList = () => {
+// Functional Component 
+
+// props is short for "properties" that allow you to pass information to a component 
+// so the component can display or use that information 
+// props = {} 
+
+const UserList = (props) => {
+  const [users, setUsers] = useState(props.users)
+  const [selectedUser, setSelectedUser] = useState(null)
+
+  const deleteUser = (index) => {
+    // Filter method removes users from the list 
+    const newUsers = users.filter((user, i) => i !== index)
+    setUsers(newUsers)
+  }
+
+  const selectUser = (index) => {
+    setSelectedUser(users[index])
+  }
+
   return (
-    <div>UserList</div>
+    <div>
+
+      <h3>Selected User</h3>
+      <p>{selectedUser}</p>
+
+      {/* We only want to display the selected user if they exist */}
+      {/* We check if there is a selected user and then we display */}
+      { selectedUser && 
+        <div>
+            <h3>Selected User</h3>
+            <p>{selectedUser}</p>
+        </div>
+      }
+
+      <h1>List of Users</h1>
+      { users.map((user, index) => 
+        <div key={index}>
+          <p>{user}</p>
+
+          {/* <button onClick={() => deleteUser(index)}>Delete User</button> */}
+          <ReusableButton title='Delete User' handleClick={() => deleteUser(index)}/>
+
+          <br />
+
+          <ReusableButton title='Select User' handleClick={() => selectUser(index)}/>
+        </div>
+      )}
+
+    </div>
   )
+}
+
+UserList.defaultProps = {
+  users: ["Jake Default", "Sally Default", "Sarah Default"]
 }
 
 export default UserList
